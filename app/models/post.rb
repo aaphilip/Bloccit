72a09d41c51dcc159930e3be10810bdf988c801b
Alfred Philipson <fredphilipson@gmail.com>
2016-01-07 03:25:12 +0000

    Assignment 31 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ActiveRecord::Base
    has_many :comments
+
+    def self.spam_filter
+      self.all.each_with_index do | post, index |
+        if index % 5 == 0
+          post.update_attribute(:title, "SPAM")
+        end
+      end
+    end
 end
+
  
